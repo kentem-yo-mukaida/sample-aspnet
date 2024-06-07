51828b2d3039bab3140bf9e8e620ebe1688587f4
Mukaida <yo-mukaida@kentem.co.jp>
2024-06-07 17:55:35 +0900

    css

---
+++ ASPNET_Sample/Scripts/sample2.js
@@ -0,0 +1,11 @@
+/// <reference path="sample.ts" />
+var Index = /** @class */ (function () {
+    function Index() {
+        this.sample = new SampleClass();
+    }
+    Index.prototype.show = function () {
+        this.sample.showMessage();
+    };
+    return Index;
+}());
+//# sourceMappingURL=sample2.js.map
