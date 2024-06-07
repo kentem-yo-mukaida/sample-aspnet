04ba9fe02f1316071fb64d77b427b4423422f8f0
Mukaida <yo-mukaida@kentem.co.jp>
2024-06-07 17:33:20 +0900

    追加

---
+++ ASPNET_Sample/Scripts/sample.js
@@ -0,0 +1,9 @@
+var SampleClass = /** @class */ (function () {
+    function SampleClass() {
+    }
+    SampleClass.prototype.showMessage = function () {
+        alert('sample.');
+    };
+    return SampleClass;
+}());
+//# sourceMappingURL=sample.js.map
---
+++ ASPNET_Sample/Scripts/sample.ts
@@ -0,0 +1,5 @@
+﻿class SampleClass {
+    public showMessage() {
+        alert('sample.');
+    }
+}
